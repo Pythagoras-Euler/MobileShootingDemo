3fcf8c2956bddc5d6e288820fff486e7074a5b5a
Gaoyu-Liu <Gaoyu.Liu@outlook.com>
2022-05-22 05:01:53 +0800

    First playable version

--- app/src/main/java/com/example/smartphoneshootingdemo/FullscreenActivity.kt
+++ app/src/main/java/com/example/smartphoneshootingdemo/FullscreenActivity.kt
@@ -9,22 +9,23 @@ import android.os.Bundle
 import android.os.Handler
 import android.util.DisplayMetrics
 import android.util.Log
+import android.util.TypedValue
 import android.view.*
-import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.MotionEventCompat
-import com.example.smartphoneshootingdemo.databinding.ActivityFullscreenBinding
-import com.example.viewtest2.ViewDisplay.CircleCanvas
-import com.example.smartphoneshootingdemo.data.shooting_data
 import com.example.smartphoneshootingdemo.data.shooting_data.getter_score
 import com.example.smartphoneshootingdemo.data.shooting_data.setter_score
+import com.example.smartphoneshootingdemo.databinding.ActivityFullscreenBinding
+import com.example.viewtest2.ViewDisplay.CircleCanvas
+import java.lang.Math.abs
 import java.util.*
+import kotlin.math.sqrt
 
 
-//TODO  解决频闪问题 规范得分系统
+//TODO  得分判定 宽度适配
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
@@ -66,22 +67,30 @@ class FullscreenActivity : AppCompatActivity() {
 
     private val hideRunnable = Runnable { hide() }
 
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private val delayHideTouchListener = View.OnTouchListener { view, motionEvent ->
-        when (motionEvent.action) {
-            MotionEvent.ACTION_DOWN -> if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS)
-            }
-            MotionEvent.ACTION_UP -> view.performClick()
-            else -> {
-            }
-        }
-        false
-    }
+    var currentCirclePosX :Float = 0.toFloat()
+    var currentCirclePosY :Float = 0.toFloat()
+    var currentCircleLen : Float = 0.toFloat()
+
+    var screenWX = 0
+    var screenHY = 0
+
+
+//    /**
+//     * Touch listener to use for in-layout UI controls to delay hiding the
+//     * system UI. This is to prevent the jarring behavior of controls going away
+//     * while interacting with activity UI.
+//     */
+//    private val delayHideTouchListener = View.OnTouchListener { view, motionEvent ->
+//        when (motionEvent.action) {
+//            MotionEvent.ACTION_DOWN -> if (AUTO_HIDE) {
+//                delayedHide(AUTO_HIDE_DELAY_MILLIS)
+//            }
+//            MotionEvent.ACTION_UP -> view.performClick()
+//            else -> {
+//            }
+//        }
+//        false
+//    }
 
     @SuppressLint("ClickableViewAccessibility", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -105,7 +114,7 @@ class FullscreenActivity : AppCompatActivity() {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        binding.dummyButton.setOnTouchListener(delayHideTouchListener)
+//        binding.dummyButton.setOnTouchListener(delayHideTouchListener)
 
         val viewGroup = layoutInflater.inflate(R.layout.activity_fullscreen, null) as ViewGroup
         mCircleCanvas = CircleCanvas(this) //  创建CircleCanvas（画布类）对象
@@ -114,28 +123,28 @@ class FullscreenActivity : AppCompatActivity() {
             mCircleCanvas,
             ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT)
         )
+        getScreenWH(this)
         setContentView(viewGroup)
 
 
-
-
     }
-    fun getScreenWH(context: Context) {
+    fun getScreenWH(context: Context) {//TODO 设好宽高默认值或者调好从这个函数
         var displayMetrics = DisplayMetrics()
         //获取windowManager的方式，如果是activity，则可以通过activity.windowManager直接取得，即上边获取dpi中的那样
         var windowManager: WindowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
         windowManager.defaultDisplay.getMetrics(displayMetrics)
-        var screenW = displayMetrics.widthPixels
-        var screenH = displayMetrics.heightPixels
-        Log.e("---", "-----w:$screenW==h:$screenH");
+        screenWX = displayMetrics.widthPixels
+        screenHY = displayMetrics.heightPixels
+//        Log.e("---", "-----w:$screenW==h:$screenH");
     }
 
 
-    //  开始随机绘制圆形（第一个按钮的单击事件）
+    //  开始随机绘制圆形
     fun DrawRandomCircle() {
+        var edge = 20
         val random = Random()
-        val randomX = (10 + random.nextInt(3000)).toFloat() //  随机生成圆心横坐标（100至200）
-        val randomY = (10 + random.nextInt(1000).toFloat() )//  随机生成圆心纵坐标（100至200）
+        val randomX = (edge + random.nextInt(screenWX - 2* edge)).toFloat() //  随机生成圆心横坐标（100至200）
+        val randomY = (edge + random.nextInt(screenHY - 10* edge).toFloat() )//  随机生成圆心纵坐标（100至200）
         val randomRadius = (40 + random.nextInt(100)).toFloat() //  随机生成圆的半径（20至60）
         var randomColor = 0
 
@@ -153,40 +162,59 @@ class FullscreenActivity : AppCompatActivity() {
         val circleInfo = CircleCanvas.CircleInfo()
         circleInfo.x = randomX
         circleInfo.y = randomY
+
         circleInfo.radius = randomRadius
         circleInfo.color = randomColor
+
+        //获取状态栏高度
+        val res = resources.getIdentifier("status_bar_height","dimen","android")
+        var statusBarHeight = 0
+        if (res > 0) {
+            statusBarHeight = getResources().getDimensionPixelSize(res)
+        }
+        //获取actionBar
+        val tv =  TypedValue()
+        var actionBarHeight = 0
+        if (this.theme.resolveAttribute(android.R.attr.actionBarSize, tv, true)) {
+            actionBarHeight = TypedValue.complexToDimensionPixelSize(tv.data, this.resources.displayMetrics)   //   高度问题感谢CSDN博主  版权声明：本文为CSDN博主「poegeeon」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。 原文链接：https://blog.csdn.net/poegeeon/article/details/118947225
+        }
+
+        currentCirclePosX = circleInfo.x
+        currentCirclePosY = circleInfo.y + statusBarHeight
+        currentCircleLen = circleInfo.radius
+
         mCircleCanvas?.mCircleInfos?.add(circleInfo) //  将当前绘制的实心圆信息加到List对象中
         mCircleCanvas!!.invalidate() //  使画布重绘
-        binding.currentScoreDisplay.text = "目前得分："
 
     }
 
-    //  清空画布（第二个按钮的单击事件）
+    //  清空画布（屏幕的单击事件）（清一次画一个）
     @SuppressLint("SetTextI18n", "CutPasteId")
-    fun ClearCircle() {
+    fun ClearCircle() {//日常清零
         mCircleCanvas?.mCircleInfos?.clear() //  清除绘制历史
         mCircleCanvas!!.invalidate() //  使画布重绘
-        val background_score: TextView = findViewById<TextView>(R.id.fullscreen_content)
-        background_score.text = "上次得分：${getter_score()}"
-        setter_score( target_score = 0)
-        val current_score: TextView = findViewById<TextView>(R.id.currentScoreDisplay)
-        current_score.text = "@strings/score_display_init"
-//        Toast.makeText(this@FullscreenActivity, "CLICKED ", Toast.LENGTH_SHORT).show()
-
-
+//        val background_score: TextView = findViewById<TextView>(R.id.fullscreen_content)
+//        background_score.text = "上次得分：${getter_score()}"
+//        setter_score( target_score = 0)
+//        val current_score: TextView = findViewById<TextView>(R.id.currentScoreDisplay)
+//        current_score.text = "@strings/score_display_init"
+//        Toast.makeText(this@FullscreenActivity, "计数已清空", Toast.LENGTH_SHORT).show()
     }
 
     @SuppressLint("SetTextI18n", "CutPasteId")
-    fun ClearCircle(view: View) {
-        mCircleCanvas?.mCircleInfos?.clear() //  清除绘制历史
-        mCircleCanvas!!.invalidate() //  使画布重绘
+    fun ClearCircle(view: View) {//按钮清零
+        ClearCircle()
+        RestartTXT()
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun RestartTXT(){
         val background_score: TextView = findViewById<TextView>(R.id.fullscreen_content)
         background_score.text = "上次得分：${getter_score()}"
-        setter_score( target_score = 0)
+        Toast.makeText(this@FullscreenActivity, "计数已清空 上次得分：${getter_score()}", Toast.LENGTH_SHORT).show()
+        setter_score( target_score = 0 )
         val current_score: TextView = findViewById<TextView>(R.id.currentScoreDisplay)
-        current_score.text = "目前得分：${getter_score()}"
-        Toast.makeText(this@FullscreenActivity, "计数已清空 ${getter_score()}", Toast.LENGTH_SHORT).show()
-
+        current_score.text = "目前得分：${getter_score()}"//按任意处开始
     }
 
     override fun onResume() {
@@ -268,6 +296,27 @@ class FullscreenActivity : AppCompatActivity() {
          */
         private const val UI_ANIMATION_DELAY = 300
     }
+
+    fun checkInput(touchPosX:Float, touchPosY: Float, circlePosX: Float, circlePosY: Float, circleLen:Float): Boolean {//后续更新可以把返回值改为Int以支持多重分数计算，或者分数处理就在这里进行也可以
+        val distX = abs(touchPosX - circlePosX)
+        val distY = abs(touchPosY - circlePosY)
+        val dist = sqrt(distX.toDouble() * distX.toDouble() + distY.toDouble() * distY.toDouble())
+
+//        Toast.makeText(this@FullscreenActivity, "计数已清空 ${getter_score()}", Toast.LENGTH_SHORT).show()
+
+        if(dist <= circleLen)
+        {
+//            setter_score(change_score = 1);//计分放到外面，结构先留着
+            return true
+        }
+        else
+        {
+            return false
+        }
+    }
+
+
+
     private var mVelocityTracker: VelocityTracker? = null
 
     @SuppressLint("SetTextI18n")
@@ -282,32 +331,46 @@ class FullscreenActivity : AppCompatActivity() {
                 mVelocityTracker = mVelocityTracker ?: VelocityTracker.obtain()
                 // Add a user's movement to the tracker.
                 mVelocityTracker?.addMovement(event)
+
+
+                val x = event.x.toFloat()
+                val y = event.y.toFloat()
+
+                if(checkInput(x,y,currentCirclePosX,currentCirclePosY,currentCircleLen)){
+                    ClearCircle()
+                    DrawRandomCircle()
+                    setter_score(change_score = 1)
+                }
+                else
+                {
+                    ClearCircle()
+                    DrawRandomCircle()
+//                    RestartTXT()//TODO 失败惩罚的事回头再改，先交再说 bug来源为初始化
+                }
             }
 
-            MotionEvent.ACTION_MOVE -> {
-                mVelocityTracker?.apply {
-                    val pointerId: Int = event.getPointerId(event.actionIndex)
-                    addMovement(event)
-                    // When you want to determine the velocity, call
-                    // computeCurrentVelociandroid:launchMode="singleTask" android:screenOrientation="portrait">ty(). Then call getXVelocity()
-                    // and getYVelocity() to retrieve the velocity for each pointer ID.
-                    computeCurrentVelocity(1000)
+//            MotionEvent.ACTION_MOVE -> {
+//                mVelocityTracker?.apply {
+//                    val pointerId: Int = event.getPointerId(event.actionIndex)
+//                    addMovement(event)
+//                    // When you want to determine the velocity, call
+//                    // computeCurrentVelociandroid:launchMode="singleTask" android:screenOrientation="portrait">ty(). Then call getXVelocity()
+//                    // and getYVelocity() to retrieve the velocity for each pointer ID.
+//                    computeCurrentVelocity(1000)
                     // Log velocity of pixels per second
                     // Best practice to use VelocityTrackerCompat where possible.
 //                    Toast.makeText(this@FullscreenActivity, "CLICKED ;点击位置：（${MotionEventCompat.getX(event,pointerId)} ,${MotionEventCompat.getY(event, pointerId)}）; 移动位置：（${getXVelocity(pointerId)} ;${getYVelocity(pointerId)}）", Toast.LENGTH_SHORT).show()
 //                    Log.d("", "X velocity: ${getXVelocity(pointerId)}")//X从左到右，Y从上到下
 //                    Log.d("", "Y velocity: ${getYVelocity(pointerId)}")
 
-                    ClearCircle()
-                    DrawRandomCircle()
 
-                }
-            }
-            MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
-                // Return a VelocityTracker object back to be re-used by others.
-                mVelocityTracker?.recycle()
-                mVelocityTracker = null
-            }
+//                }
+//            }
+//            MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+//                // Return a VelocityTracker object back to be re-used by others.
+//                mVelocityTracker?.recycle()
+//                mVelocityTracker = null
+//            }
         }
 
 //        setter_score(change_score = 1)
